a35bf346375b8511db4c131ee86f2235e803fdb4
Chris Severns <severnsc@gmail.com>
2016-10-04 16:26:42 -0400

    Add Caesar Cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(string, shift)
+	alphabet = ('a'..'z').to_a
+	letters = string.split("")
+    letters.collect! do |letter|
+		if letter != " "
+			index = alphabet.index(letter.downcase)
+			if /[A-Z]/.match(letter) != nil
+				letter = alphabet[index-shift].upcase
+			else
+				letter = alphabet[index-shift]
+			end
+        else
+          letter = " "
+		end
+	end
+    word = letters.join
+	puts word
+end
+caesar_cipher("THis is A lonGer StrINg",8)
