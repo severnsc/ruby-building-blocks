aeb301d12220cbef5f192147f8ef8917772e7514
Chris Severns <severnsc@gmail.com>
2016-10-10 14:01:07 -0400

    Add block support to the bubble sort method to create bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,4 +18,29 @@ def bubble_sort array
 	end
 	print array
 end
-bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by array
+	passes = 1
+	not_sorted = true
+	while not_sorted
+		not_sorted = false
+		i=0
+		while i<array.length-passes
+			value = yield array[i], array[i+1]
+			if value > 0
+				larger = array[i]
+				smaller = array[i+1]
+				array[i] = smaller
+				array[i+1] = larger
+				not_sorted = true
+			end
+			i+=1
+		end
+		passes+=1
+	end
+	print array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+	left.length-right.length
+end
